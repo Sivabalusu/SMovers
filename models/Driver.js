61df7e27be12e1c8d0b86df8244786839f42a06d
Siva Balusu <Sbalusu1882@conestogac.on.ca>
2020-10-09 12:07:41 -0400

    Created Driver Model

--- models/Driver.js
+++ models/Driver.js
@@ -1,1 +1,36 @@
 //Driver model for MongoDB containing fields / properties of a Driver
+const mongoose = require('mongoose');
+
+const DriverSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  rate: {
+    type: Number,
+    required:true,
+  },
+  rating:{
+      type:Number,
+      min:1,
+      max:5,
+  },
+  totalTrips:{
+      type:Number,
+  },
+  licenseIssuedDate:{
+      type:Date,
+      required:true,
+  }
+});
+
+module.exports = Driver = mongoose.model('driver', DriverSchema);
