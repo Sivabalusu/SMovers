2af099b4054272e346d7a25c45d24eba14251488
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-12-03 22:51:24 -0500

    Deployin

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ app.use('/api/drivers', require('./routes/api/drivers'));
 app.use('/api/bookings', require('./routes/api/bookings'));
 
 //Serve static assets Prod
-if(process.env.NODE_ENV === 'production'){
-    
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'build')));
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
 }
 const PORT = process.env.PORT || 5000;
 
